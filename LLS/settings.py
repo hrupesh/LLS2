24966af1dc1cbe7420ae30172fff4bcaf8e80638
hrupesh <rcashok0@gmail.com>
2020-01-11 10:34:12 +0530

    123

--- LLS/settings.py
+++ LLS/settings.py
@@ -82,7 +82,6 @@ DATABASES = {
     }
 }
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -122,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
