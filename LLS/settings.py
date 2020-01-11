5b3f3124d8d69c4c5a2a8652ec66bf259282f9ab
hrupesh <rcashok0@gmail.com>
2020-01-11 10:24:15 +0530

    ...

--- LLS/settings.py
+++ LLS/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l^%csy$6i8nt!@ylp^7*91fu+=2sz4(_ty46^ij-jk!spwensz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*','127.0.0.1']
 
